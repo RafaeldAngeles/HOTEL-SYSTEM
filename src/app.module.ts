b9e122c8f5edb58458e507913ba5ff8d26ae4cd8
Rafael <rafael.angeles.toledano@gmail.com>
2025-07-09 11:25:14 -0300

    {chore}: inserting initial project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { RoomModule } from './room/room.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    RoomModule,
+    ReservationModule,
+  ],
 })
 export class AppModule {}
