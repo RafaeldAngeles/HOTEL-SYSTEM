37f737f94f2d9e72d4a44da4b84f33123cb39fde
Rafael <rafael.angeles.toledano@gmail.com>
2025-07-09 13:08:32 -0300

    {feat(db)}: new table in database named room

--- src/reservation/entities/reservation.entity.ts
+++ src/reservation/entities/reservation.entity.ts
@@ -1,1 +1,21 @@
-export class Reservation {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RoomType } from './room-type.enum';
+
+@Entity()
+export class Reservation {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  number: number;
+
+  @Column({
+    type: 'enum',
+    enum: RoomType,
+    default: RoomType.Single,
+  })
+  type: RoomType;
+
+  @Column()
+  price: number;
+}
---
+++ src/reservation/entities/room-type.enum.ts
@@ -0,0 +1,5 @@
+export enum RoomType {
+  Single = 'single',
+  Double = 'double',
+  Suite = 'suite',
+}
