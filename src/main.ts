13697e985424520947e33f0cb4ea64e47f829ce0
Rafael Angeles <rafael.angeles.toledano@gmail.com>
2025-12-09 16:09:43 -0300

    {feat}: add swagger to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle("API Docs")
+  .setDescription("Documentação da API")
+  .setVersion("1.0")
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document)
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  console.log('Server running on http://localhost:3000');
+  console.log('📘 SWAGGER DOCS: http://localhost:3000/docs');
 }
 bootstrap();
