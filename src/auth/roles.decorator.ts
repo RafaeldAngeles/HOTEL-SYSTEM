7a3f6490005ced4f5f6fd264ffb31bb854f222e1
Rafael Angeles <rafael.angeles.toledano@gmail.com>
2025-12-18 12:04:40 -0300

    {feat}: add permissions to the  project and admin routes

--- src/auth/roles.decorator.ts
+++ src/auth/roles.decorator.ts
@@ -3,6 +3,3 @@ import { SetMetadata } from "@nestjs/common";
 
 export const ROLES_KEY = "roles";
 export const Roles = (...roles: String[]) => SetMetadata(ROLES_KEY, roles)
-
-export const IS_PUBLIC_KEY = "isPublic";
-export const Public = ()=> SetMetadata(IS_PUBLIC_KEY, true)
